008472dbc5fc07f3e46b648c6338f669d797b44e
suleiman99hesham <suleimanhesham99@gmail.com>
2020-05-24 22:35:10 +0200

    add website folder

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\Education\career path\python\pluralsight\django courses\Django- Getting Started\pracctices\meetin_planner_django_course\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
