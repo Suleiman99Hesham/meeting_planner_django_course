cd4f512eda3386bade7c7038bf3c06e99ebfddf5
suleiman99hesham <suleimanhesham99@gmail.com>
2020-05-25 09:58:45 +0200

    create meeting class model and make required migrations

--- meeting_planner/website/views.py
+++ meeting_planner/website/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 # Create your views here.
 
 
 def welcome(request):
-    return HttpResponse("Welcome to the meeting planner")
+    return HttpResponse("<html> <head>Meeting planner</head> <body><h1>Welcome to the meeting planner</h1> </body></html>")
+
+
+def date(request):
+    return HttpResponse("this page was served at "+str(datetime.now()))
+
+
+def about(request):
+    return HttpResponse("my name is suleiman, i'm django developer, i love python")
